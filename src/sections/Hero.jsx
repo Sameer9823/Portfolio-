01b0785246ffbdead7a1239d383e41d2b67beecb
Sameer Selokar <sameerselokar924@gmail.com>
2025-04-12 23:45:57 +0530

    Implement Hero section with dynamic text and images

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
+  const words = [
+    { text: "Ideas", imgPath: "/images/ideas.svg" },
+    { text: "Concepts", imgPath: "/images/concepts.svg" },
+  ];
   return (
-    <div>Hero</div>
-  )
+    <section id="hero" className="relative overflow-hidden">
+      <div className="absolute top-0 left-0 z-10">
+        <img src="/images/bg.png" alt="gfhjkl" />
+      </div>
+
+      <div className="hero-layout">
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="her-text">
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word) => (
+                      <span
+                        key={word.text}
+                        className="flex items-center md:gap-3 gap-1 pb-2 "
+                      >
+                        <img
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                          src={word.imgPath}
+                          alt={word.text}
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+              <h1>into Real Projects</h1>
+              <h1>that Delivsr Results</h1>
+            </div>
+          </div>
+        </header>
+      </div>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
