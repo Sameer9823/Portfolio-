f33c0157f35390f260783bf519e5ce7262fce18e
Sameer Selokar <sameerselokar924@gmail.com>
2025-04-13 15:47:54 +0530

    Add FeatureCard component and integrate it into the App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './sections/Hero'
 import ShowcaseSection from './sections/ShowcaseSection'
 import NavBar from './components/NavBar'
 import LogoSection from './components/LogoSection'
+import FeatureCard from './sections/FeatureCard'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <ShowcaseSection/>
       <LogoSection/>
+      <FeatureCard/>
     </>
   )
 }
